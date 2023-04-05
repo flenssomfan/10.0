80bcc1bcb83d48659baf57342e705f609e1a7f44
Nikita Prekrasnyi <topdoterinua@gmail.com>
2023-04-05 16:02:35 +0300

    first commit

---
+++ main.js
@@ -0,0 +1,13 @@
+function main(a = 2, b = 3, c) {
+  var sumResult = sum(a, b);
+  if (typeof c === "function") {
+    return c(sumResult);
+  }
+  return sumResult;
+}
+
+function sum(a, b) {
+  return a + b;
+}
+
+console.log(main());
